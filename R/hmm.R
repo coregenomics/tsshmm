ae28288faaf687a481a26a7b553c879083e55734
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2020-06-13 09:08:43 -0400

    STY: Indent using 4 spaces for BiocCheck coding style

--- R/hmm.R
+++ R/hmm.R
@@ -50,8 +50,9 @@ hmm <- function(signal, bg, ranges) {
     check_valid_hmm_reads(signal)
     check_valid_hmm_reads(bg)
     stopifnot(is(ranges, "GRanges"))
-    mendoapply(hmm_by_strand, stranded(signal), stranded(bg),
-               MoreArgs = list(ranges = ranges))
+    mendoapply(
+        hmm_by_strand, stranded(signal), stranded(bg),
+        MoreArgs = list(ranges = ranges))
 }
 
 ## Rely on annotations for now.  Based on the data below, more careful work is
