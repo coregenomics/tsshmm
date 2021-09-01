bb1b2510d78b7d0a0593ea5d6431c6d80fe90e3f
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2021-08-31 22:39:53 -0400

    DEV: Emacs flycheck can search headers from the bundled GHMM

--- src/.dir-locals.el
+++ src/.dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c-mode
-  (flycheck-gcc-include-path . ("/usr/share/R/include"))))
+  (flycheck-gcc-include-path . ("/usr/share/R/include" "ghmm-0.9-rc3"))))
