e5eec44f5b51a05da9837e277f3eec5c23e7e627
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2021-10-05 10:16:40 -0400

    TST: Move prom_dist tests to viterbi helpers

--- tests/testthat/test-viterbi.R
+++ tests/testthat/test-viterbi.R
@@ -1,4 +1,30 @@
+tol <- 10 ^ (-1 * 3:8)
+## These expected values are calculated in the vignette appendix.
+x <- c(28, 39, 50, 60, 71, 82)
+stopifnot(length(tol) == length(x))
+n_low <- floor(x / 10) * 10
+n_low <- ifelse(n_low == x, floor((x - 1) / 10) * 10, n_low)
+stopifnot(all(n_low < x))
+n_high <- n_low + 10
+n_high <- ifelse(n_high == x, n_high + 10, n_high)
+stopifnot(all(n_high > x))
+
 context("viterbi helpers")
+
+test_that("prom_dist is valid for several tolerances", {
+    model <- new("TSSHMM")
+    for (i in seq_along(tol)) {
+        expect_equal(prom_dist(model, tol = tol[i]), x[i])
+    }
+})
+
+test_that("prom_dist detects tolerance too stringent", {
+    model <- new("TSSHMM")
+    for (i in seq_along(tol)) {
+        expect_error(prom_dist(model, tol = tol[i], n = n_low[i]))
+        expect_silent(prom_dist(model, tol = tol[i], n = n_high[i]))
+    }
+})
 
 test_that("stranded always splits into + and - levels", {
     expect_equal(lengths(stranded(GRanges("chr1:100:+"))), c("+" = 1, "-" = 0))
