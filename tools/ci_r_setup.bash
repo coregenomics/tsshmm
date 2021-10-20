50a52531442d46e3283802bd1406614364d0597a
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2021-10-20 13:43:09 -0400

    BLD: Bundle generated files

--- tools/ci_r_setup.bash
+++ tools/ci_r_setup.bash
@@ -8,11 +8,3 @@ local({
             Ncpus = parallel::detectCores())
 })
 EOF
-# Don't track the GHMM source files in version control, because running
-# configure modifies many tracked files which creates version control noise.
-# Additionally, the GHMM tarball guarantees the integrity of the source, and
-# makes us disciplined in applying patches making for easier upstream
-# contributions.
-pushd src/ || exit
-tar -xf ghmm-0.9-rc3.tar.gz
-popd || exit
